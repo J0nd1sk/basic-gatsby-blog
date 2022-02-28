87944d6c7b8630dc676bf8955cdd6471bbd87c92
RichPalladino <engineer@packetdeli.com>
2022-02-27 17:55:29 -0800

    Added necessary plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -21,6 +23,20 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`
       },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },  
     },
   ],
 }
