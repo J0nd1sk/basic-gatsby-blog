d3e1e07a67354d504d743ff72f67504b1ebbbb4a
RichPalladino <engineer@packetdeli.com>
2022-03-21 23:55:55 -0700

    automated page generation with gatsby-node.js

---
+++ gatsby-node.js
@@ -0,0 +1,52 @@
+/*
+
+exports.createPages = async function({actions, graphql}) {
+  const {data} = await graphql`
+    query{
+      allMdx(sort: {fields: frontmatter___date, order: DESC })
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+            id
+          }
+        }
+    }
+
+
+  `
+
+  // Create paginated pages for posts.
+
+  const postsPerPage = 2
+
+  const numPages = Math.ceil(data.allMdx.edges.length / postsPerPage)
+
+  Array.from({length: numPages}).forEach((_, i) => {
+    actions.createPages({
+      path: i === 0 ? '/' : '/${i +1}',
+      component: require.resolve("./src/templates/allposts.js"),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        curPage: i + 1,
+      }
+    })
+  })
+
+  data.allMdx.edges.forEach(edge => {
+    const slug = edge.node.frontmatter.slug
+    const id = edge.node.id
+    actions.createPages({
+      path: slug,
+      component: require.resolve('./src/templates/singlePost.js'),
+      context: {id},
+    })
+  })
+
+
+}
+
+*/ 
