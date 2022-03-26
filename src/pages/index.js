333eda14a8520ec811b8342ec1b94abfdde226ef
RichPalladino <engineer@packetdeli.com>
2022-03-25 23:21:24 -0700

    modified the index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
 
 export default function Home() {
-  return <div><h1>Hello world!</h1></div>
+  return <div>
+    <h1>Hello world!</h1>
+    <p>You've arrived at a place that doesn't exist - except in your imagination...</p>
+  </div>
 }
